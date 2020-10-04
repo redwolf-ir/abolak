5928afbba99f086df8e3edf04a35b89d6feb2b3e
redwolf <redwolf_ir@outlook.com>
2020-10-04 14:15:09 +0330

    slider be auto, slider btns changed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-new fullpage("#fullpage", {
+var slideTimeout;
+
+$("#fullpage").fullpage({
   navigation: true,
   responsiveWidth: 700,
   anchors: ["welcome", "works", "contact"],
   parallax: true,
   scrollOverflow: true,
   slidesNavigation: true,
-  onLeave: function (origin, destination, direction) {
-    console.log("Leaving section" + origin.index);
+  css3: true,
+  afterRender: function () {
+    //on page load, start the slideshow
+    slideTimeout = setInterval(function () {
+      $.fn.fullpage.moveSlideRight();
+    }, 7000);
+  },
+
+  onLeave: function (index, direction) {
+    //after leaving section 1 (home) and going anywhere else, whether scrolling down to next section or clicking a nav link, this SHOULD stop the slideshow and allow you to navigate the site...but it does not
+    if (index == "1") {
+      console.log("on leaving the slideshow/section1");
+      clearInterval(slideTimeout);
+    }
   },
 });
-
 $(window).on("load", function () {
   $(".loader-wrapper").fadeOut("slow");
 });
